9d33988c6f51405bcd030a1b0d4eeaf8e7ce096d
rriarys <rriarys@gmail.com>
2026-05-30 17:12:20 +0300

    feat(app): add user input reading and validation

--- Tracker.App/Program.cs
+++ Tracker.App/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Tracker App is running!");
+
+const int MaxLength = 200;
+
+while (true)
+{
+    Console.Write("Enter your command > ");
+    string? userInputCommand = Console.ReadLine();
+
+    // Normalize and validate the user input
+    if (string.IsNullOrEmpty(userInputCommand))
+    {
+        Console.WriteLine("Command cannot be empty. Please try again.");
+        continue;
+    }
+    else if (userInputCommand == "exit")
+    {
+        Console.WriteLine("Exiting the application...");
+        break;
+    }
+    else
+    {
+        userInputCommand = userInputCommand.Trim().ToLower();
+
+        if (userInputCommand.Length > MaxLength)
+        {
+            Console.WriteLine($"Command cannot exceed {MaxLength} characters. Please try again.");
+            continue;
+        }
+    }
+
+    Console.WriteLine($"You entered: {userInputCommand}");
+}
